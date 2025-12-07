43393fdc432d44f95181dc96b34a0b89493cd914
Vitaliy Morzhov <voxfoot@gmail.com>
2025-12-07 21:13:58 +0500

    refactor: improve success logging by calculating relative path before joining

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,7 +2,8 @@ import path from "path";
 
 export const logger = {
   success: (pathName) => {
-    const result = path.join(path.basename(process.cwd()), pathName);
+    const relativePath = path.relative(process.cwd(), pathName);
+    const result = path.join(path.basename(process.cwd()), relativePath);
     console.log(`Page was successfully downloaded into ${result}'`);
   },
   error: (message) => {
