c3d74f2854723d817b25b744002d9ab4eee7724c
Vitaliy Morzhov <voxfoot@gmail.com>
2026-01-15 11:28:45 +0500

    refactor: writeFile

--- src/utils/writeFile.js
+++ src/utils/writeFile.js
@@ -1,53 +1,23 @@
 import fs from 'fs/promises'
 import path from 'path'
 
-const ensureDirExists = (dir) => {
-  return new Promise((resolve, reject) => {
-    fs.access(dir, fs.constants.F_OK)
-      .then(() => resolve())
-      .catch(() => {
-        fs.mkdir(dir, { recursive: true })
-          .then(() => resolve())
-          .catch((error) => {
-            if (error.code === 'EACCES') {
-              reject(
-                new Error(`Недостаточно прав для создания директории: ${dir}`),
-              )
-            }
-            else {
-              reject(
-                new Error(
-                  `Не удалось создать директорию ${dir}: ${error.message}`,
-                ),
-              )
-            }
-          })
-      })
-  })
-}
-
 export const writeFile = (outputDir, fileName, data) => {
-  return new Promise((resolve, reject) => {
-    ensureDirExists(outputDir)
-      .then(() => {
-        const filePath = path.join(outputDir, fileName)
-        fs.writeFile(filePath, data)
-          .then(() => resolve(filePath))
-          .catch((error) => {
-            if (error.code === 'EACCES') {
-              reject(
-                new Error(`Недостаточно прав для записи файла: ${filePath}`),
-              )
-            }
-            else {
-              reject(
-                new Error(
-                  `Не удалось записать файл ${filePath}: ${error.message}`,
-                ),
-              )
-            }
-          })
-      })
-      .catch(reject)
-  })
+  const filePath = path.join(outputDir, fileName)
+
+  return fs.access(outputDir, fs.constants.F_OK)
+    .catch(() => fs.mkdir(outputDir, { recursive: true })
+      .catch((error) => {
+        if (error.code === 'EACCES') {
+          throw new Error(`Недостаточно прав для создания директории: ${outputDir}`)
+        }
+        throw new Error(`Не удалось создать директорию ${outputDir}: ${error.message}`)
+      }))
+    .then(() => fs.writeFile(filePath, data))
+    .then(() => filePath)
+    .catch((error) => {
+      if (error.code === 'EACCES') {
+        throw new Error(`Недостаточно прав для записи файла: ${filePath}`)
+      }
+      throw new Error(`Не удалось записать файл ${filePath}: ${error.message}`)
+    })
 }
